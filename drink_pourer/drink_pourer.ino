411839bf31e6fc8b209c576184a6cc42fa54a611
VedaantKuchhal <vedaantk22@gmail.com>
2021-11-16 04:19:29 -0500

    Modify code to solenoid valves instead of stepper motors

--- drink_pourer/drink_pourer.ino
+++ drink_pourer/drink_pourer.ino
@@ -1,11 +1,8 @@
 /*Example sketch to control a stepper motor with A4988 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
 
 // Define stepper motor connections and steps per revolution:
-#define dirPin 2
-#define stepPin 3
-#define stepsPerRevolution 200
+#define SOLENOID_PIN 13
 
-int command = 1;
 
 // Declare function to pour water.
 void pour_water(int amount);
@@ -15,41 +12,12 @@ void setup() {
   Serial.begin(9600);
   
   // Declare pins as output:
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  pinMode(SOLENOID_PIN, OUTPUT);
 }
 
 void loop() {
-//  // Arbitrary stepper motor motion - to be changed when mechanics are known.
-//  // Set the spinning direction clockwise:
-//  digitalWrite(dirPin, HIGH);
-//
-//  // Spin the stepper motor 1/4 revolution clockwise quickly to open the nozzle:
-//  for (int i = 0; i < stepsPerRevolution / 4; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(500);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(500);
-//  }
-//
-//  // Pause for specified amount of time.
-//  delay(1000);
-//
-//  // Set the spinning direction counterclockwise:
-//  digitalWrite(dirPin, LOW);
-//
-//  // Spin the stepper motor 1/4 revolution counterclockwise quickly to close the nozzle:
-//  for (int i = 0; i < stepsPerRevolution / 4; i++) {
-//    // These four lines result in 1 step:
-//    digitalWrite(stepPin, HIGH);
-//    delayMicroseconds(500);
-//    digitalWrite(stepPin, LOW);
-//    delayMicroseconds(500);
-//  }
-//  // Pause for specified amount of time.
-//  delay(1000);
-//  // Check if serial channel is available.
+
+// Check if serial channel is available.
   if (Serial.available() > 0) {
     // read the incoming byte:
     command = Serial.parseInt();
@@ -63,33 +31,13 @@ void loop() {
 void pour_water(int amount){
   int time_interval = amount*50; // Add function to convert the amount of liquid into the time to pour in milliseconds (using pressure etc.)
   
-  // Arbitrary stepper motor motion - to be changed when mechanics are known.
-  // Set the spinning direction clockwise:
-  digitalWrite(dirPin, HIGH);
-
-  // Spin the stepper motor 1/4 revolution clockwise quickly to open the nozzle:
-  for (int i = 0; i < stepsPerRevolution / 4; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500);
-  }
-
+  // Open the valve for water.
+  digitalWrite(SOLENOID_PIN, HIGH);
+  Serial.println("Water is pouring...");
   // Pause for specified amount of time.
   delay(time_interval);
 
-  // Set the spinning direction counterclockwise:
-  digitalWrite(dirPin, LOW);
-
-  // Spin the stepper motor 1/4 revolution counterclockwise quickly to close the nozzle:
-  for (int i = 0; i < stepsPerRevolution / 4; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500);
-  }
-
+  // Close the valve.
+  digitalWrite(SOLENOID_PIN, LOW);
   Serial.println("Your water has been poured!");
 }
